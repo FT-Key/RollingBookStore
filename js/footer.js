417386b69317f442e965527892c9bb3ec5a61cba
FT-Key <fr4nc0t2@gmail.com>
2024-07-17 18:40:13 -0300

    Se agrega redireccionamiento a sobreNosotros desde el footer y se soluciona problema de redireccionamiento a 404

--- js/footer.js
+++ js/footer.js
@@ -1,18 +1,23 @@
 // Definir el contenido del footer
 
 export function agregarFooter() {
-  const hrefError = location.href === "/" || location.href === "/index.html" ? "pages/error404.html" : "error404.html";
+  const hrefError = location.pathname === "/" || location.pathname === "/index.html" ? "pages/error404.html" : "error404.html";
+  const hrefSobreNosotros = location.pathname === "/" || location.pathname === "/index.html" ? "pages/sobreNosotros.html" : "sobreNosotros.html";
 
   const footerHTML = `
         <footer class="text-center">
           <div class="container">
             <div class="row">
-              <div class="col-6 mb-3">
-                <h5>Sobre Nosotros</h5>
-                <p>Información sobre la empresa</p>
+              <div class="col-12 col-md-5 mb-3">
+                <a href="${hrefSobreNosotros}" class="fs-4">Sobre Nosotros</a>
+                <p class="fs-6">RollingBookStore es tu tienda en línea para libros. Encuentra bestsellers, clásicos y más, con promociones exclusivas y entrega a domicilio. ¡Tu próxima gran lectura te espera!</p>
+              </div>
+
+              <div class="col-12 col-md-2">
+
               </div>
               
-              <div class="col-6 mb-3">
+              <div class="col-12 col-md-5 mb-3">
                 <h5>Síguenos</h5>
                 <a href="${hrefError}" class="me-2"><i class="fab fa-facebook-f"></i> Facebook</a>
                 <a href="${hrefError}" class="me-2"><i class="fab fa-twitter"></i> Twitter</a>
